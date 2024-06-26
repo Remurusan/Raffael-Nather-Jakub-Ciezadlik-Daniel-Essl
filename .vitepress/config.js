0af0754dabb32007bb3bfbb6d24786ccac2ee15d
Remurusan <raffael.nather@gmail.com>
2024-06-26 10:38:02 +0200

    Initial commit

---
+++ .vitepress/config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Simple VitePress Site",
+  description: "Training Page for Cloud Computing Course",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projekt starten
+Damit die Web Applikation ausgeführt werden kann, müssen zuerst alle notwendigen Dependencies installiert werden. Ausführung bspw. mithilfe der Command Shell im Projektordner:
+
+``` shell
+npm i 
+```
+
+Danach kann das Projekt im Development Modus ausgeführt werden:
+
+```
+npm run docs:dev
+```
